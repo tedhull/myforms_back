ed8dd5bd1e837001352bf999d380ad26a9585104
tedhull <jedi.f.sh@gmail.com>
2025-06-18 19:07:44 +0400

    entrypoint updated

--- start.sh
+++ start.sh
@@ -6,8 +6,7 @@ until php bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
   sleep 2
 done
 
-# Run migrations
-php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:migrations:diff
+php bin/console doctrine:migrations:migrate
 
-# Start built-in PHP server
 php -S 0.0.0.0:80 -t public
